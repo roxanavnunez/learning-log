f1da33d495c2e1add3e720d8a05a38122bf7d93c
Roxana Nuñez <roxana.nunez.nav@gmail.com>
2025-10-25 19:32:08 -0600

    feat(models): Format short entries

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -20,4 +20,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """Return a simple string representing the entry."""
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
